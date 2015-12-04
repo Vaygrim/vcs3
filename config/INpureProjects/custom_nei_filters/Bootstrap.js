49b62eb1e7e00de0e0fef4f51fde1e0cdfcb373f
Trunks9809 <pheonix9809@googlemail.com>
2015-12-04 08:28:39 +0000

    v0.0.7
    New configs for the V0.0.7 release.

--- config/INpureProjects/custom_nei_filters/Bootstrap.js
+++ config/INpureProjects/custom_nei_filters/Bootstrap.js
@@ -5,6 +5,5 @@ var ForgeMicroblock_enabled = true;
 var ExtraUtilities_enabled = true;
 var BuildCraft_enabled = true;
 var Bibliocraft_enabled = true;
-var AE2_enabled = true;
-var Tcon_enabled = true;
 var MFR_enabled = true;
+var StorageDrawers_enabled = true;
